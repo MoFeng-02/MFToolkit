69ccdbeda933a7e9507d4cebd010da69bdcf0eda
MF <mf.02@qq.com>
2025-03-09 23:11:05 +0800

    提交一次修复自动生成

--- GeneratorDemo/DI/Demoa.cs
+++ GeneratorDemo/DI/Demoa.cs
@@ -1,17 +1,17 @@
-﻿//using MFToolkit.AutoAttribute.DependencyInjection;
+﻿using MFToolkit.AutoAttribute.DependencyInjection;
 
-//namespace GeneratorDemo.DI;
+namespace GeneratorDemo.DI;
 
-//public interface IService
-//{
-//}
-//public interface IService2
-//{
-//}
-//[AutoInject]
-//[AutoInject<IService>(lifetime: Lifetime.Scoped)]
-//[AutoInject<IService2>("1", lifetime: Lifetime.Scoped)]
-//[AutoInject(Lifetime.Singleton)]
-//public class Demoa : IService, IService2
-//{
-//}
+public interface IService
+{
+}
+public interface IService2
+{
+}
+[AutoInject]
+[AutoInject<IService>(lifetime: Lifetime.Scoped)]
+[AutoInject<IService2>("1", lifetime: Lifetime.Scoped)]
+[AutoInject(Lifetime.Singleton)]
+public class Demoa : IService, IService2
+{
+}
